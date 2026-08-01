f0797c6597f522d7a484263cbae2f4782de9b271
Jason Walker <JasonW94@gmail.com>
2026-08-01 10:32:37 -0400

    WIP

--- examples/gen-example.py
+++ examples/gen-example.py
@@ -6,7 +6,7 @@ with open("big-example.txt", "w+") as f:
     depth = 0
 
     ops = "+-*/"
-    for i in range(0, 20000000):#1000000):
+    for i in range(0, 20000000):
         op = random.randrange(0, 4)
         f.write(ops[op])
         if random.randrange(0, 10) < 2:
